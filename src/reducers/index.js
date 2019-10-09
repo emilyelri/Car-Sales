1b9bacc1f8b2ef24f826ad507f1a1a8cce4b25bd
Emily_Richard <richard.emily.e@gmail.com>
2019-10-09 14:30:02 -0400

    update

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,1 +1,38 @@
-export {reducer} from './reducer';
+import { REMOVE_ITEM, ADD_ITEM } from '../actions';
+
+const initialState = {
+    additionalPrice: 0,
+    car: {
+      price: 26395,
+      name: "2019 Ford Mustang",
+      image: "https://cdn.motor1.com/images/mgl/0AN2V/s1/2019-ford-mustang-bullitt.jpg",
+      features: []
+    },
+    store: [
+      { id: 1, name: "V-6 engine", price: 1500 },
+      { id: 2, name: "Racing detail package", price: 1500 },
+      { id: 3, name: "Premium sound system", price: 500 },
+      { id: 4, name: "Rear spoiler", price: 250 }
+    ]
+  };
+
+export const reducer = (state = initialState, action) => {
+
+  switch(action.type) {
+    case ADD_ITEM:
+      console.log("Add item triggered.")
+      return {
+        ...state,
+        additionalPrice = state.additionalPrice + action.payload.price,
+        car: {...state.car, features: [...state.car.features, action.payload]}
+      }
+    case REMOVE_ITEM:
+      console.log("Remove item triggered.")
+      return {
+        ...state,
+        additionalPrice: state.additionalPrice - action.payload.price,
+        car: {...state.car, features: state.car.features.filter(item => !(item.id===action.payload.id))}}
+    default:
+      return state;
+  }
+};
