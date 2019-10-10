b7e3bc31a651e0a39a2d25f6ef9ae314752cbfc1
Emily_Richard <richard.emily.e@gmail.com>
2019-10-09 20:47:16 -0400

    tried to connect components, need bug fixes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { connect } from 'react-redux';
 import Header from './components/Header';
 import AddedFeatures from './components/AddedFeatures';
 import AdditionalFeatures from './components/AdditionalFeatures';
@@ -7,33 +6,18 @@ import Total from './components/Total';
 
 const App = () => {
 
-  const removeFeature = item => {
-    // dispatch an action here to remove an item
-  };
-
-  const buyItem = item => {
-    // dispatch an action here to add an item
-  };
-
   return (
     <div className="boxes">
       <div className="box">
-        <Header car={state.car} />
-        <AddedFeatures car={state.car} />
+        <Header />
+        <AddedFeatures />
       </div>
       <div className="box">
-        <AdditionalFeatures store={state.store} />
-        <Total car={state.car} additionalPrice={state.additionalPrice} />
+        <AdditionalFeatures />
+        <Total />
       </div>
     </div>
   );
 };
 
-const mapStateToProps = state => {
-  return {
-    car: state.car,
-    additionalPrice: state.additionalPrice
-  };
-};
-
-export default connect(mapStateToProps, { })(App);
+export default App;
--- src/index.js
+++ src/index.js
@@ -2,15 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
-import {createStore} from 'redux';
+import { createStore } from 'redux';
 import { Provider } from 'react-redux';
-import { reducer } from './reducers/index';
+import { carReducer } from './reducers/index';
 
 import 'bulma/css/bulma.css';
 import './styles.scss';
 
 
-const store = createStore(reducer);
+const store = createStore(carReducer);
+console.log("From within index.js! Store: ", store)
 
 
 const rootElement = document.getElementById('root');
